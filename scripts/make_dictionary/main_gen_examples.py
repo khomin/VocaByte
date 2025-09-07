d3563ebf35fe8cb5b9b3180bf6fb2d255c6c072f
kernel panic <hominvladimir@gmail.com>
2025-09-07 20:13:09 +0400

    Update main_gen_examples.py

--- scripts/make_dictionary/main_gen_examples.py
+++ scripts/make_dictionary/main_gen_examples.py
@@ -46,14 +46,15 @@ def handle_llama_batch(word_str):
     # llamam input
     request = "i have a list of words and i want you to give a json object for each word." \
         "Return a single json array containing an object for each word, using single line for single object" \
+        "here's json object example:\n" \
+        "{\"word\": \"example\",\"freq\": \"a number, the word's rank\", \"examples\":[ <PUT EXAMPLES HERE> ]}\n" \
         "Here are the rules for each JSON object:" \
         "- \"word\": - the word itself." \
-        "- \"freq\": - the rank of the given word in WikiText corpus, if can't be found use -1" \
-        "- \"examples\": - you have to generate 15 examples for the given word, in an array" \
-        "here's json object example:\n" \
-        "{\"word\": \"example\",\"freq\": \"a number, the word's rank\", \"examples\":[]}\n" \
-        "don't place comments inside json (like // Not found in WikiText corpus)\n" \
-        "Here is the list of the words for the task: \"%s\"\n" % (word_str)
+        "- \"freq\": The word's rank (a number), using -1 if unknown" \
+        "- \"examples\": - you need to generate 15 examples of a sentence with the given word\n" \
+        "do not include any extra text, comments, or explanations inside the JSON\n" \
+        "surround the main json with two ```, to separate it from comments\n" \
+        "here is the list of the words for the task: \"%s\"\n" % (word_str)
 
     # Send a single request
     response = ollama.chat(
@@ -74,11 +75,22 @@ def handle_llama_batch(word_str):
     sub_message = sub_message.replace("\\", "'")
     # parse every json object
     sub_message = sub_message.replace("json", "", 1)
-    sub_message = sub_message.replace("...", "", 1)
-    sub_message = sub_message.replace(",\n  \n]\n", "]")
-    sub_message = sub_message.replace("},\n...\n", "}")
-    json_obj = json.loads(sub_message)
-    return json_obj
+    sub_message = sub_message.replace("...", "")
+    sub_message = sub_message.replace("\n", "")
+    last_symbols = sub_message[len(sub_message)-4:-1]
+    # when llm adds extra command in the end
+    index = last_symbols.find(",")
+    if index != -1:
+        sub_message[index] = " "
+    # when llm doesn't add ']' in the end
+    if sub_message[-1] != ']':
+        sub_message += "]"
+    try:
+        json_obj = json.loads(sub_message)
+        return json_obj
+    except Exception as e:
+        print(f"Invalid json: {e}")
+    return None
 
 def write_json(json_obj, path, file_name):
     try:
@@ -88,12 +100,12 @@ def write_json(json_obj, path, file_name):
     try:
         # out_file = open(os.path.join(path, file_name), 'w')
         # json.dump(json_obj, out_file, indent=4)
-        data = {
-            "name": "Alice",
-            "age": 30,
-            "isStudent": False,
-            "courses": ["Math", "Science"]
-        }
+        # data = {
+        #     "name": "Alice",
+        #     "age": 30,
+        #     "isStudent": False,
+        #     "courses": ["Math", "Science"]
+        # }
         # Dump the data to a JSON file with indentation for readability
         with open(os.path.join(path, file_name), 'w') as f:
             json.dump(json_obj, f, indent=4) 
@@ -141,10 +153,14 @@ for dict_file in dict_files:
             break
         obj = json_to_process[obj_key]
         queue.append(obj)
-        if len(queue) >= 50:
+        if len(queue) >= 20:
             words_str = queue_format_to_string(queue)
             queue = []
             res_json = handle_llama_batch(words_str)
+            if res_json is None:
+                # failed, can generate for these batch later
+                queue = []
+                break
             for res_obj in res_json:
                 key = res_obj["word"]
                 obj_modified = {}
@@ -161,159 +177,3 @@ for dict_file in dict_files:
     write_json(json_out, out_dict_path, dict_file)
 
     print('done, total len: {}', len(json_out))
-
-    # while len(json_dict) > 0:
-    #     keys = list(json_dict.keys())
-    #     min_len = 0
-    #     if len(json_dict) < BATCH_SIZE:
-    #         min_len = len(json_dict)
-    #     else:
-    #         min_len = BATCH_SIZE
-    #     batch_keys = keys[:min_len]
-    #     words_str = ", ".join(batch_keys)
-        
-    #     print("Processing batch:", batch_keys)
-
-    #     # Remove the processed items from the original dictionary
-    #     for key in batch_keys:
-    #         del json_dict[key]
-
-
-#     jsonStr = f.read()
-#     jsonV = json.loads(jsonStr)
-
-# frequency = []
-
-# # get 100k words
-# f = open(frequencyFile, "r", errors='replace')
-# raw_entire = f.read().splitlines()
-
-# # filter comments
-# for word in raw_entire:
-#     if word[0] != '#':
-#         frequency.append(word)
-#     else:
-#         print("skip word: " + word)
-
-
-# # open db
-# db = Db()
-# db.open_connection(dbPath)
-        
-# # copy dictionary in db and add frequency index
-# for file in dictFiles:  
-#     f = open(dictFolder + file, "r", errors='replace')
-#     jsonStr = f.read()
-#     jsonV = json.loads(jsonStr)
-    
-#     # find this for in the frequency list
-#     for word in jsonV:
-#         freq = 0
-#         transcript = ""
-#         try:
-#             transcript = ipa.convert(word)
-#             freq = frequency.index(word)
-#         except:
-#             freq = -1
-#         db.insert_record(word, freq, json.dumps(jsonV[word]), transcript)
-
-# print("done")
-
-
-
-# top_words = wordfreq.top_n_list('en', 1000000)
-# word_the_rank = top_words.index('the') + 1 
-# word_roll_up_rank = top_words.index('roll') + 1  # Adding 1 because list indices start at 0
-# word_roll_up_rank = top_words.index('consecrate') + 1  # Adding 1 because list indices start at 0
-# kowtow = top_words.index('kowtow') + 1  # Adding 1 because list indices start at 0
-
-# def zipf_to_rank(zipf_freq, language='en'):
-#     # Total number of tokens in the corpus (this value might need to be adjusted based on corpus specifics)
-#     N = 30_000_000_000
-    
-#     # Calculate the frequency from the Zipf frequency
-#     f = 10 ** (zipf_freq - 9) * N
-    
-#     # Estimate the rank
-#     rank = N / f
-    
-#     return rank
-
-# freq = wordfreq.zipf_frequency('roll up', 'en')
-# freq = wordfreq.zipf_frequency('lpsds', 'en')
-# freq = wordfreq.zipf_frequency('the', 'en')
-# freq = wordfreq.zipf_frequency('consecrate', 'en')
-# freq = wordfreq.zipf_frequency('prowl', 'en')
-
-
-# rank_the = zipf_to_rank(wordfreq.zipf_frequency('the', 'en'))
-# rank_roll_up = zipf_to_rank(wordfreq.zipf_frequency('roll up', 'en'))
-# roll = zipf_to_rank(wordfreq.zipf_frequency('roll', 'en'))
-# prowl = zipf_to_rank(wordfreq.zipf_frequency('prowl', 'en'))
-# consecrate = zipf_to_rank(wordfreq.zipf_frequency('consecrate', 'en'))
-
-
-# Download the NLTK data
-# nltk.download('words')
-# nltk.download('genesis')
-
-# word_list2 = []
-
-# Load the NLTK word list
-# word_list = set(nltk.corpus.words.words())
-
-# word_list_gen = nltk.corpus.genesis.words()
-
-# Create a Counter object to store the frequency of each word
-# word_freq = Counter()
-
-# Iterate over the word list and count the frequency of each word
-# for word in word_list:
-#     # word_freq[word] += 1
-#     word_list2.append(word)
-
-# Print the top 10 most frequent words
-# print(word_freq.most_common(10))
-
-# v = word_list2[0]
-# word_list2.reverse()
-# count = 0
-# for i in word_list2:
-#     if i == 'thunder':
-#         print(i)
-#     count += 1
-
-# print(v)
-
-
-
-# db = Db()
-# # db.create_connection(os.getcwd() + '/database.db')
-
-# db.open_connection(os.getcwd() + '/database.db')
-# db.insert_record()
-
-# f = open(os.getcwd() + '/data_sources/' + it, "r", errors='replace')
-# raw_entire = f.read().splitlines()
-
-# formatted = self.__formatNewLines(raw_entire)
-
-# entire.append(self.__splitByPunctuation(formatted))
-
-# # fill_db = CreateDb()
-# # number_of_lines = fill_db.start(source_storage)
-
-# print("fill database done, number of lines: " + str(number_of_lines))
-
-# source_storage = SourceStorage()
-# sources_len = source_storage.getSize()
-
-# if sources_len == 0:
-#     print("sources list is empty\nPlease check the book directory")
-# else:
-#     print("sources_len size: " + str(sources_len))
-
-#     fill_db = FillDb()
-#     number_of_lines = fill_db.start(source_storage)
-
-#     print("fill database done, number of lines: " + str(number_of_lines))
